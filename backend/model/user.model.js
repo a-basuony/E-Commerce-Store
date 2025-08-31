ead6f892b77336b93caf6fe75e817f41d4e298e9
Ahmed Basuony <A.Basuony.social@gmail.com>
2025-08-31 13:25:03 +0300

    add user schema and hash the password

---
+++ backend/model/user.model.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [4, "password must be at least 4 characters long"],
+    },
+    cartItems: [
+      {
+        quantity: {
+          type: Number,
+          required: [true, "quantity is required"],
+          default: 1,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: [true, "product is required"],
+        },
+      },
+    ],
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+  },
+  { timestamps: true } // createdAt , updatedAt
+);
+
+const User = mongoose.model("User", userSchema);
+
+// pre-save hook to hash the password before saving to database
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// compare
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export default User;
--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoute);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log("server is running on http://localhost:" + PORT);
   connectDB();
 });
