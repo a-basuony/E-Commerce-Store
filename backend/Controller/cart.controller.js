d72fb0f7c93cda6d07ab04033f9fa765407223f0
Ahmed Basuony <A.Basuony.social@gmail.com>
2025-09-04 18:59:23 +0300

    adding get cart products logic to cart

--- backend/Controller/cart.controller.js
+++ backend/Controller/cart.controller.js
@@ -1,3 +1,26 @@
+import Product from "../model/product.model.js";
+
+export const getCartProducts = async (req, res) => {
+  try {
+    const products = await Product.find({ _id: { $in: req.user.cartItems } }); // find all products
+    // add quantity to each product
+    const cartItems = products.map((product) => {
+      const item = req.user.cartItems.find(
+        (cartItem) => cartItem.id.toString() === product._id.toString()
+      ); // find the cart item with the same id as the product
+      return {
+        ...product.toJSON(),
+        quantity: item.quantity,
+      };
+    });
+
+    res.status(200).json(cartItems);
+  } catch (error) {
+    console.log("Error in getCartProducts", error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
 export const addToCart = async (req, res) => {
   try {
     const { productId } = req.body;
--- backend/Routes/cart.route.js
+++ backend/Routes/cart.route.js
@@ -4,11 +4,13 @@ import {
   addToCart,
   removeAllFromCart,
   updateQuantity,
+  getCartProducts,
 } from "../Controller/cart.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+router.get("/", protectRoute, getCartProducts);
 router.post("/", protectRoute, addToCart);
 router.post("/", protectRoute, removeAllFromCart);
 router.put("/:id", protectRoute, updateQuantity);
