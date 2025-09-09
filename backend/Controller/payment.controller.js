f88911fd79dab484b18536d5faf4666dc8478925
Ahmed Basuony <A.Basuony.social@gmail.com>
2025-09-09 22:04:57 +0300

    adding logic for creating a new coupon

--- backend/Controller/payment.controller.js
+++ backend/Controller/payment.controller.js
@@ -60,7 +60,11 @@ router.post("create-checkout-session", protectRoute, async (req, res) => {
       },
     });
 
-    res.json({ url: session.url });
+    if (totalAmount >= 20000) {
+      await createNewCoupon(req.user._id);
+    }
+
+    res.status(200).json({ id: session.id, totalAmount: totalAmount / 100 });
   } catch (error) {}
 });
 
@@ -70,6 +74,18 @@ async function createStripeCoupon(discountPercentage) {
     duration: "once",
   });
   return coupon.id;
+}
+
+async function createNewCoupon(userId) {
+  const newCoupon = new Coupon({
+    code: "GIFT" + Math.random().toString(36).substring(2, 8).toUpperCase(),
+    discountPercentage: 10,
+    expirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+    userId: userId,
+  });
+
+  await newCoupon.save();
+  return newCoupon;
 }
 
 export default router;
